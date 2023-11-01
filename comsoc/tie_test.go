941ee2c26fbf33605ba9c5a87df5e283573bbe45
Baptiste Buvron <baptiste.buvron@gmail.com>
2023-11-01 17:17:13 +0100

    Feats: - added test for SCF Factory with TieBreak

--- comsoc/tie_test.go
+++ comsoc/tie_test.go
@@ -6,25 +6,13 @@ import (
 	"testing"
 )
 
-type MockProfile struct{}
-
-func (p MockProfile) Voters() []string {
-	return []string{"Alice", "Bob", "Charlie"}
-}
-
-type MockCount Count
-
-func (c MockCount) Get(alt Alternative) int {
-	return c[alt]
-}
-
 func mockSWF(p Profile) (Count, error) {
-	count := MockCount{
+	count := Count{
 		Alternative(0): 3,
 		Alternative(1): 3,
 		Alternative(2): 3,
 	}
-	return Count(count), nil
+	return count, nil
 }
 
 func mockTieBreak(alt []Alternative) (Alternative, error) {
@@ -48,5 +36,29 @@ func TestSWFFactory(t *testing.T) {
 	// Check if the result is sorted correctly
 	if !reflect.DeepEqual(result, expectedResult) {
 		t.Errorf("Result does not match expected order. Got %v, expected %v", result, expectedResult)
+	}
+}
+
+func mockSCF(p Profile) ([]Alternative, error) {
+	// For simplicity, returning a fixed set of alternatives
+	return []Alternative{Alternative(0), Alternative(1), Alternative(2)}, nil
+}
+
+func TestSCFFactory(t *testing.T) {
+	// Arrange
+	factory := SCFFactory(mockSCF, mockTieBreak)
+	expectedResult := Alternative(2)
+
+	// Act
+	result, err := factory(Profile{})
+
+	// Assert
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	// Check if the result matches the expected alternative
+	if !reflect.DeepEqual(result, expectedResult) {
+		t.Errorf("Result does not match expected alternative. Got %v, expected %v", result, expectedResult)
 	}
 }
